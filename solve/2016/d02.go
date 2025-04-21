28626279c0d2767f45c506527bb165e9dfdd2143
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2025-04-21 15:58:31 +0100

    feat: 2016d02, 2023d01

--- solve/2016/d02.go
+++ solve/2016/d02.go
@@ -2,6 +2,7 @@ package solve2016
 
 import (
 	"errors"
+	"strings"
 
 	"aoc/solve"
 )
@@ -13,12 +14,75 @@ func (d Day2) Coords() solve.SolutionCoords {
 	return solve.SolutionCoords{Year: 2016, Day: 2}
 }
 
+const off = '.'
+
+func decode(keypad []string, instructions []string, x, y int) string {
+	var result strings.Builder
+	for _, line := range instructions {
+		for _, command := range line {
+			x, y = move(keypad, command, x, y)
+		}
+		result.WriteByte(keypad[y][x])
+	}
+	return result.String()
+}
+
+func move(keypad []string, direction rune, x, y int) (int, int) {
+	switch direction {
+	case 'L':
+		if keypad[y][x-1] != off {
+			x--
+		}
+	case 'R':
+		if keypad[y][x+1] != off {
+			x++
+		}
+	case 'U':
+		if keypad[y-1][x] != off {
+			y--
+		}
+	case 'D':
+		if keypad[y+1][x] != off {
+			y++
+		}
+	}
+	return x, y
+}
+
 func (d Day2) Part1(input string) (string, error) {
-	return "", errors.New("Not implemented")
+	data := strings.TrimSpace(input)
+
+	keypad := []string{
+		".....",
+		".123.",
+		".456.",
+		".789.",
+		".....",
+	}
+
+	if keypad[2][2] != '5' {
+		return "", errors.New("invalid keypad configuration")
+	}
+
+	instructions := strings.Split(data, "\n")
+	return decode(keypad, instructions, 2, 2), nil
 }
 
 func (d Day2) Part2(input string) (string, error) {
-	return "", errors.New("Not implemented")
+	data := strings.TrimSpace(input)
+
+	keypad := []string{
+		".......",
+		"...1...",
+		"..234..",
+		".56789.",
+		"..ABC..",
+		"...D...",
+		".......",
+	}
+
+	instructions := strings.Split(data, "\n")
+	return decode(keypad, instructions, 1, 3), nil
 }
 
 func init() {
--- solve/2023/d01.go
+++ solve/2023/d01.go
@@ -1,8 +1,10 @@
 package solve2023
 
 import (
+	"strconv"
+	"strings"
+
 	"aoc/solve"
-	"errors"
 )
 
 type Day1 struct {
@@ -12,12 +14,26 @@ func (d Day1) Coords() solve.SolutionCoords {
 	return solve.SolutionCoords{Year: 2023, Day: 1}
 }
 
+func (d Day1) applyReplacer(r *strings.Replacer, input string) (result int) {
+	for _, s := range strings.Fields(string(input)) {
+		s = r.Replace(r.Replace(s))
+		result += 10 * int(s[strings.IndexAny(s, "123456789")]-'0')
+		result += int(s[strings.LastIndexAny(s, "123456789")] - '0')
+	}
+	return result
+}
+
 func (d Day1) Part1(input string) (string, error) {
-	return "", errors.New("Not implemented")
+	sum := d.applyReplacer(strings.NewReplacer(), input)
+
+	return strconv.Itoa(sum), nil
 }
 
 func (d Day1) Part2(input string) (string, error) {
-	return "", errors.New("Not implemented")
+	sum := d.applyReplacer(strings.NewReplacer("one", "o1e", "two", "t2o", "three", "t3e", "four",
+		"f4r", "five", "f5e", "six", "s6x", "seven", "s7n", "eight", "e8t", "nine", "n9e"), input)
+
+	return strconv.Itoa(sum), nil
 }
 
 func init() {
