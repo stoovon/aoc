265c92a14b1a33c4c94d42388bfd42f240803818
Stuart Murray <10003354+stoovon@users.noreply.github.com>
2025-04-21 20:22:07 +0100

    feat: 2019d01

--- solve/2019/d01.go
+++ solve/2019/d01.go
@@ -1,8 +1,10 @@
 package solve2019
 
 import (
+	"strconv"
+	"strings"
+
 	"aoc/solve"
-	"errors"
 )
 
 type Day1 struct {
@@ -12,12 +14,35 @@ func (d Day1) Coords() solve.SolutionCoords {
 	return solve.SolutionCoords{Year: 2019, Day: 1}
 }
 
+func fuel(mass int) int {
+	return mass/3 - 2
+}
+
+func (d Day1) run(input string) (int, int) {
+	totalPart1, totalPart2 := 0, 0
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+
+	for _, line := range lines {
+		mass, _ := strconv.Atoi(line)
+
+		totalPart1 += mass/3 - 2
+
+		for f := fuel(mass); f > 0; f = fuel(f) {
+			totalPart2 += f
+		}
+	}
+
+	return totalPart1, totalPart2
+}
+
 func (d Day1) Part1(input string) (string, error) {
-	return "", errors.New("Not implemented")
+	part1, _ := d.run(input)
+	return strconv.Itoa(part1), nil
 }
 
 func (d Day1) Part2(input string) (string, error) {
-	return "", errors.New("Not implemented")
+	_, part2 := d.run(input)
+	return strconv.Itoa(part2), nil
 }
 
 func init() {
